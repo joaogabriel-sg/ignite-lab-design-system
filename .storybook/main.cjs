6b97b35eae927317b5fff5cbf43ed8bcbadda609
João Gabriel <main.joaogabrielsg@gmail.com>
2022-10-15 19:05:17 -0300

    feat: create sign in page and run tests

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,7 +12,9 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+    interactionsDebugger: true,
   },
+  staticDirs: ["../public"],
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
       config.base = "/ignite-lab-design-system/";
